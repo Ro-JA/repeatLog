c59e5568c1055595b8f5f669c88dc98137607f9d
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-08-11 02:09:55 +0900

    repeat log

--- app/src/main/java/com/example/repeatlog/MainActivity.kt
+++ app/src/main/java/com/example/repeatlog/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.repeatlog
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logging()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "Application onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "Application onResume")
+    }
+
+}
+
+fun logging() {
+    Log.i(TAG, "Hello wold!")
 }
