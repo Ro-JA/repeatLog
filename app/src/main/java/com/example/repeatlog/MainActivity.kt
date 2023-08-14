ca40d707b44ba5870b5940d8ad4304f8c4775d9a
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-08-14 22:48:51 +0900

    work

--- app/src/main/java/com/example/repeatlog/MainActivity.kt
+++ app/src/main/java/com/example/repeatlog/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.repeatlog
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-const val TAG = "lifecykel"
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,31 +14,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.i(TAG, "Application onStart")
+        Log.i("LifyCycel","Application onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i(TAG, "Application onResume")
+        Log.i("LifyCycel", "Application onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i(TAG, "Application onPause")
+        Log.i("LifyCycel", "Application onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i(TAG, "Application onStop")
+        Log.i("LifyCycel", "Application onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i(TAG, "Application onDestroy")
+        Log.i("LifyCycel", "Application onDestroy")
     }
 
 }
 
 fun logging() {
-    Log.i(TAG, "Hello wold!")
+    Log.i("LifyCycel", "Hello wold!")
 }
